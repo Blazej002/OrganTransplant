22f4412af6cf71f6b50cafe0e4e8a5c231defcf5
Blazej002 <141903573+Blazej002@users.noreply.github.com>
2023-11-29 11:46:24 +0100

    Ave

---
+++ OrganTransplant/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrganTransplant
+{
+    internal class Person
+    {
+        public string _name;
+        public int _age;
+        public string _bloodType;
+        public bool _smokes;
+        public bool _alcoLast24;
+
+        public Person(string name, int age, string bloodType, bool smokes, bool alcoLast24)
+        {
+            _name = name;
+            _age = age;
+            _bloodType = bloodType;
+            _smokes = smokes;
+            _alcoLast24 = alcoLast24;
+        }
+    }
+}
--- OrganTransplant/Program.cs
+++ OrganTransplant/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace OrganTransplant
+﻿using System.Xml.XPath;
+
+namespace OrganTransplant
 {
     internal class Program
     {
@@ -15,50 +17,93 @@
             var Per2 = new
             {
                 name = "Kåre",
-                bloodType = "A+",
-                age = 55,
+                bloodType = "A-",
+                age = 61,
                 Smokes = true,
                 AlkoholInLast24H = false
             };
-
-
+            
             Console.WriteLine("Det har vært en akutt ulykke og Bernt ligger på sykehuset!!!");
             Thread.Sleep(1000);
             Console.WriteLine("Han trenger en ny Nyre!");
             Console.ReadKey();
             Console.Clear();
+
+
+
+            Console.WriteLine("Patient name?");
+            var pName = Console.ReadLine();
+
+            Console.WriteLine("Patient age?");
+            var pAge = Console.ReadLine();
+            var pAgeToInt = Convert.ToInt32(pAge);
+
+            Console.WriteLine("Patient bloodtype?");
+            var pBloodType = Console.ReadLine();
+
+            Console.WriteLine("Has patient been drinking in the last 24h??");
+            Console.WriteLine("1. Yes  2. No");
+            bool pDrinking = Console.ReadLine() == "1";
+
+            Console.WriteLine("Does the patient smoke?");
+            Console.WriteLine("1. Yes  2. No");
+            bool pSmokes = Console.ReadLine() == "1";
+
+            Console.Clear();
+            Console.WriteLine("Donors name?");
+            var dName = Console.ReadLine();
+
+            Console.WriteLine("Donors age?");
+            var dAge = Console.ReadLine();
+            var dAgeToInt = Convert.ToInt32(dAge);
+
+            Console.WriteLine("Donors blood type?");
+            var dBloodType = Console.ReadLine();
+
+            Console.WriteLine("Has donor been drinking in the last 24h??");
+            Console.WriteLine("1. Yes  2. No");
+            var dDrinking = Console.ReadLine() == "1";
+
+            Console.WriteLine("Does the donor smoke?");
+            Console.WriteLine("1. Yes  2. No");
+            var dSmokes = Console.ReadLine() == "1";
+
+            var patient = new Person(pName, pAgeToInt, pBloodType, pSmokes, pDrinking);
+            var donor = new Person(dName, dAgeToInt, dBloodType, dSmokes, dDrinking);
+
             var exit = false;
             var checkAge = false;
             var checkBlood = false;
-            var checkAlko = false;
+            Console.Clear();
             while (!exit)
             {
-                var Age = checkAge ? Per1.age : (int?)null;
-                var Age2 = checkAge ? Per2.age : (int?)null;
-                var pass = checkAge ? checkAges(Per1.age): (string?)null;
-                var pass2 = checkAge ? checkAges(Per2.age) : (string?)null;
+                var Age = checkAge ? patient._age : (int?)null;
+                var Age2 = checkAge ? donor._age : (int?)null;
+                var pass = checkAge ? checkAges(patient._age) : (string?)null;
+                var pass2 = checkAge ? checkAges(donor._age) : (string?)null;
 
-                var Blood = checkBlood ? Per1.bloodType : (string?)null;
-                var Blood2 = checkBlood ? Per2.bloodType : (string?)null;
-                var blood = checkBlood ? CheckBlood(Per1.bloodType, Per2.bloodType): (string?)null;
+                var Blood = checkBlood ? patient._bloodType : (string?)null;
+                var Blood2 = checkBlood ? donor._bloodType : (string?)null;
+                var blood = checkBlood ? CheckBlood(patient._bloodType, donor._bloodType) : (string?)null;
 
-                Console.WriteLine($"Patient  - {Per1.name}              Donor - {Per2.name}");
+                Console.WriteLine($"Patient  - {patient._name}              Donor - {donor._name}");
                 Console.WriteLine($"   Age -                {Age} {pass} : {Age2} {pass2}");
                 Console.WriteLine($"   BloodType -          {Blood} {blood} : {Blood2} {blood}");
-                Console.WriteLine($"   Smokes -            {Per1.Smokes} : {Per2.Smokes}");
-                Console.WriteLine($"   Alko. in last 24h - {Per1.AlkoholInLast24H} : {Per2.AlkoholInLast24H}");
+                Console.WriteLine($"   Smokes -            {patient._smokes} : {donor._smokes}");
+                Console.WriteLine($"   Alko. in last 24h - {patient._alcoLast24} : {donor._alcoLast24}");
                 Console.WriteLine();
-                Console.WriteLine("1. Check age requerments");
-                Console.WriteLine("2. Check Blood requerments");
+                Console.WriteLine("1. Test");
+                Console.WriteLine("2. Check success rate");
                 Console.WriteLine("3. Exit");
                 var choice = Console.ReadLine();
                 switch (choice)
                 {
                     case "1":
                         checkAge=true;
+                        checkBlood = true;
                         break;
                     case "2":
-                        checkBlood = true;
+                        CheckSuccessRate(patient,donor);
                         break;
                     case "3":
                         exit = true;
@@ -68,6 +113,47 @@
                 Console.Clear();
             }
         }
+
+        static void CheckSuccessRate(Person patient, Person donor)
+        {
+            int chance = 0;
+
+            bool blood = patient._bloodType == donor._bloodType;
+            bool pAge = patient._age < 60;
+            bool dAge = donor._age < 60;
+
+            if (pAge) chance += 20;
+            else chance -= 10;
+            if (dAge) chance += 20;
+            else chance -= 10;
+
+            if (patient._smokes) chance -= 10;
+            else chance += 10;
+            if (donor._smokes) chance -= 10;
+            else chance += 10;
+
+            if (donor._alcoLast24) chance -= 20;
+            else chance += 20;
+
+            if (patient._alcoLast24) chance -= 10;
+            else chance += 20;
+
+            // Use the result of CheckBlood to influence the chance
+            string bloodResult = CheckBlood(patient._bloodType, donor._bloodType);
+            if (bloodResult == "\u2717")  // If blood types don't match
+            {
+                chance = 0;
+            }
+            else if (bloodResult == "\u2713")  // If blood types match
+            {
+                chance += 10;  // Adjust the chance accordingly
+            }
+
+            chance = Math.Max(0, Math.Min(chance, 100));
+            Console.WriteLine($"{chance}% success rate!");
+            Console.ReadKey();
+        }
+
 
         static string CheckBlood(string BloodType1, string bloodType2)
         {
